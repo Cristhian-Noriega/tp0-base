2e10f6b87512e8a5f8070b4f2673350230991400
Cristhian-Noriega <noriegacristhian57@gmail.com>
2026-03-13 17:37:39 -0300

    feat: add script to dynamically create docker-compose configurations for a server and n clients

---
+++ generar-compose.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <archivo_salida> <cantidad_clientes>"
+  exit 1
+fi
+
+cat << EOF > $1
+name: tp0
+services:
+  server:
+    container_name: server
+    image: server:latest
+    entrypoint: python3 /main.py
+    environment:
+      - PYTHONUNBUFFERED=1
+      - LOGGING_LEVEL=DEBUG
+    networks:
+      - testing_net
+EOF
+
+
+for i in $(seq 1 "$2")
+do
+cat << EOF >> "$1"
+  client${i}:
+    container_name: client${i}
+    image: client:latest
+    entrypoint: /client
+    environment:
+      - CLI_ID=${i}
+      - CLI_LOG_LEVEL=DEBUG
+    networks:
+      - testing_net
+    depends_on:
+      - server
+EOF
+done
+
+
+cat << EOF >> "$1"
+networks:
+  testing_net:
+    ipam:
+      driver: default
+      config:
+        - subnet: 172.25.125.0/24
+EOF
