c9cde331ca2a1d8236822ee407a18a846a410595
Cristhian-Noriega <noriegacristhian57@gmail.com>
2026-03-16 17:14:09 -0300

    feat: add bet struct to represent agency bets

---
+++ client/common/bet.go
@@ -0,0 +1,11 @@
+package common
+
+// Bet Struct that represents a bet from an agency
+type Bet struct {
+	Agency    int
+	FirstName string
+	LastName  string
+	Document  string
+	Birthdate string
+	Number    int
+}
