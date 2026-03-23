f47193f9dff48851661dab6694c5271bdfb7cea2
Cristhian-Noriega <noriegacristhian57@gmail.com>
2026-03-23 19:03:00 -0300

    feat: implement winner querying and response handling in protocol server

--- server/common/protocol.py
+++ server/common/protocol.py
@@ -1,4 +1,4 @@
-from common.utils import Bet
+from common.utils import Bet, load_bets, has_won
 
 
 HEADER_SIZE = 2
@@ -6,6 +6,9 @@ BYTE_SHIFT = 8
 BET_FIELD_COUNT = 6
 ACK_SUCCESS = 0x01
 ACK_FAILURE = 0x00
+BYTE_MASK = 0xFF
+NOT_READY = 0x00
+READY = 0x01
 
 def recv_exactly(conn, n) -> bytes:
     data = b''
@@ -49,4 +52,37 @@ def recv_batch(conn) -> list:
 
 def send_ack(conn, success: bool) -> None:
     conn.sendall(bytes([ACK_SUCCESS if success else ACK_FAILURE]))
+
+
+def recv_winners_query(conn) -> int:
+    """
+    It receives the ID of the agency that wants to query the winners.
+    [2 bytes: agency_id]
+    """
+    raw_agency_id = recv_exactly(conn, HEADER_SIZE)
+    agency_id = (raw_agency_id[0] << BYTE_SHIFT) | raw_agency_id[1]
+    return agency_id
+
+def send_winners(conn, agency_id: int) -> None:
+    """
+    [2 bytes: cantidad de ganadores en bytes]
+    [DNI\n DNI\n ...]
+    """
+    winners = [
+        bet.document 
+        for bet in load_bets() 
+        if has_won(bet) and bet.agency == agency_id
+    ]
     
+    count = len(winners)
+    header = bytes([count >> BYTE_SHIFT, count & BYTE_MASK])
+    payload = "\n".join(winners) + "\n" if winners else ""
+    
+    conn.sendall(bytes([READY]) + header + payload.encode('utf-8'))
+
+def send_not_ready(conn) -> None:
+    """
+    Sends to client not ready to get the winners.
+    [1 byte to tell if it's ready or not]
+    """
+    conn.sendall(bytes([NOT_READY]))
