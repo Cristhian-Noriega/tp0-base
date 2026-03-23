4ecaaa6caf964ebb398f640830c11c8dbbe2a433
Cristhian-Noriega <noriegacristhian57@gmail.com>
2026-03-23 11:18:31 -0300

    refactor: stream CSV row-by-row and split client logic

--- client/common/client.go
+++ client/common/client.go
@@ -1,9 +1,12 @@
 package common
 
 import (
+	"encoding/csv"
 	"fmt"
+	"io"
 	"net"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/op/go-logging"
@@ -52,7 +55,62 @@ func (c *Client) createClientSocket() error {
 	return nil
 }
 
-func (c *Client) StartClient(bets []Bet, quit chan os.Signal) {
+func (c *Client) readCSV(csvPath string, quit chan os.Signal) error {
+	f, err := os.Open(csvPath)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	reader := csv.NewReader(f)
+	agency, _ := strconv.Atoi(c.config.ID)
+	var chunk []Bet
+
+	for {
+		select {
+		case <-quit:
+			log.Infof("action: loop_interrupted | result: success | client_id: %v", c.config.ID)
+			return nil
+		default:
+		}
+
+		row, err := reader.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Warningf("action: read_csv | result: fail | client_id: %v | error: %v", c.config.ID, err)
+			continue
+		}
+
+		if len(row) < csvMinColumns {
+			continue
+		}
+
+		bet, err := parseBetRow(row, agency)
+		if err != nil {
+			continue
+		}
+
+		chunk = append(chunk, bet)
+
+		if len(chunk) >= c.config.BatchMaxAmount {
+			if err := c.sendChunk(chunk); err != nil {
+				return err
+			}
+			chunk = nil
+		}
+	}
+
+	if len(chunk) > 0 {
+		if err := c.sendChunk(chunk); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (c *Client) StartClient(csvPath string, quit chan os.Signal) {
 	select {
 	case <-quit:
 		log.Infof("action: loop_interrupted | result: success | client_id: %v", c.config.ID)
@@ -72,23 +130,9 @@ func (c *Client) StartClient(bets []Bet, quit chan os.Signal) {
 		log.Infof("action: close_resource | result: success | client_id: %v | resource: connection", c.config.ID)
 	}()
 
-	for i := 0; i < len(bets); i += c.config.BatchMaxAmount {
-		select {
-		case <-quit:
-			log.Infof("action: loop_interrupted | result: success | client_id: %v", c.config.ID)
-			return
-		default:
-		}
-
-		end := i + c.config.BatchMaxAmount
-		if end > len(bets) {
-			end = len(bets)
-		}
-		chunk := bets[i:end]
-		if err := c.sendChunk(chunk); err != nil {
-			log.Errorf("action: send_chunk | result: fail | client_id: %v | error: %v", c.config.ID, err)
-			return
-		}
+	if err := c.readCSV(csvPath, quit); err != nil {
+		log.Errorf("action: read_csv | result: fail | client_id: %v | error: %v", c.config.ID, err)
+		return
 	}
 }
 
